6d69bb627f3fe67145fd955e35a938d26721d4a9
mostafa <mostafameerzad@gmail.com>
2022-08-20 15:35:44 +0430

    correct heading tag levels

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,7 +4,7 @@ const Article = ({title, author, content, src}) => {
     return (
         <section className='article'>
             <img className="article-img" src={src} alt={title} />
-            <h4 className='author'>{author}</h4>
+            <h3 className='author'>{author}</h3>
             <h3 className="title">{title}</h3>
             <p className="content text-content">{content}</p>
         </section>
